68cbc748e0cec0e82dd5398657ae9244e311ac91
MahiRaunak37 <ranjanashish374757@gmail.com>
2023-07-18 17:33:45 +0530

    creating working dir

---
+++ JavaPractice/src/J3000StartingClass.java
@@ -0,0 +1,9 @@
+public class J3000StartingClass {
+    static int count;
+    public static void main(String[] args) {
+        count= 2000;
+        System.out.println("Starting class");
+        System.out.println("Current Program number:- ");
+
+    }
+}
---
+++ JavaPractice/src/J5999EndingClass.java
@@ -0,0 +1,5 @@
+public class J5999EndingClass {
+    public static void main(String[] args) {
+        System.out.println("Ending class");
+    }
+}
